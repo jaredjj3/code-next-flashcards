7d2f968bcf36d9fd6e790d0b5d551a80e30daf43
jaredjj3 <jared@jaredjohnson.dev>
2020-11-01 20:32:08 -0500

    3 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import "./style.css";
 
-export default function App() {
+export const App = () => {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Flashcards</h1>
     </div>
   );
 }
