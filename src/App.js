d796f585cf9513bc9b1237129725e47aff4e364b
jaredjj3 <jared@jaredjohnson.dev>
2020-11-01 21:13:36 -0500

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Decks } from "./Decks";
 import { Card } from "./Card";
 import { Results } from "./Results";
 import { Progress } from "./Progress";
 
 export const App = () => {
+  const [deck, setDeck] = useState(null);
+  const [cardNdx, setCardNdx] = useState(null);
+
+  const onDeckClick = (deck) => {
+    setDeck(deck);
+  };
+
   return (
     <div className="container">
       <h1>Flashcards</h1>
-      <Decks />
+      <Decks deck={deck} onDeckClick={onDeckClick} />
+      <hr />
       <Progress />
       <Card />
       <Results />
--- src/Decks.js
+++ src/Decks.js
@@ -2,15 +2,27 @@ import React from "react";
 import DECKS from "./data/DECKS.json";
 
 export const Decks = props => {
+  const onClick = deck => () => {
+    props.onDeckClick(deck);
+  };
+
   return (
     <ul className="nav nav-pills">
-      {DECKS.map(deck => (
-        <li key={deck.name} className="nav-item">
-          <a className="nav-link" href="#">
-            {deck.name}
-          </a>
-        </li>
-      ))}
+      {DECKS.map(deck => {
+        let className = "nav-link";
+
+        if (props.deck && props.deck.name === deck.name) {
+          className += " active";
+        }
+
+        return (
+          <li key={deck.name} className="nav-item">
+            <a className={className} href="#" onClick={onClick(deck)}>
+              {deck.name}
+            </a>
+          </li>
+        );
+      })}
     </ul>
   );
 };
