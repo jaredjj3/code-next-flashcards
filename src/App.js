83392cca69c8147a5dbe2df9eca4373a9b2effc0
jaredjj3 <jared@jaredjohnson.dev>
2020-11-01 20:56:19 -0500

    1 file modified, 5 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Decks } from "./Decks";
+import { Card } from "./Card";
+import { Results } from "./Results";
+import { Progress } from "./Progress";
 
 export const App = () => {
   return (
     <div>
       <h1>Flashcards</h1>
+      <Decks />
+      <Progress />
+      <Card />
+      <Results />
     </div>
   );
-}
+};
---
+++ src/Decks.js
@@ -0,0 +1,6 @@
+import React from "react";
+import DECKS from "./data/DECKS.json";
+
+export const Decks = props => {
+  return <div>Decks</div>;
+};
